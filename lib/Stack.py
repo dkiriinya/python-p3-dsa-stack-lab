565e9276b95b0419cfd80c5bcb9841fc4af129f5
dkiriinya <dkiriinya06sc@gmail.com>
2024-03-14 19:05:33 +0300

    stacks stacks and more stacks

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,43 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            print("Stack overflow")
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            print("Stack underflow")
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            print("Stack is empty")
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            top_of_stack = len(self.items) - 1
+            index = self.items.index(target)
+            if top_of_stack - index == 0:
+                return 0
+            return top_of_stack - index
+        except ValueError:
+            return -1
